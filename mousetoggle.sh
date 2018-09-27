8c96c2bf2ca0ef2adfc2a92d5a8ecfd53deadd49
PabloOyarzo <oyarzo.pablo@hotmail.com>
2018-09-27 11:09:05 -0400

    added files

---
+++ mousetoggle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOME=/home/hardwarepc/test/mousetoggle/
+DEVICE=12
+INITIAL_STATE=$(cat $HOME/.state)
+
+function toggle {
+  if [ "$INITIAL_STATE" = "disable" ]
+  then
+    xinput --disable $DEVICE
+    echo "enable" > $HOME/.state
+  else
+    xinput --enable $DEVICE
+    echo "disable" > $HOME/.state
+  fi
+}
